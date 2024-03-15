33332a7a5a0e679d518079fc381ee11939e0ad70
NivedHari <nivedhari44@gmail.com>
2024-03-16 02:20:27 +0530

    search functionality implemented

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@ import "./globals.css";
 import Header from "@/components/Header";
 import Providers from "./Providers";
 import Navbar from "@/components/Navbar";
+import SearchBox from "@/components/SearchBox";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,7 +18,8 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
         <Providers>
           <Header />
-          <Navbar/>
+          <Navbar />
+          <SearchBox />
           {children}
         </Providers>
       </body>
