c53edcba6bd643087e01eb04121922f0ba49085a
NivedHari <nivedhari44@gmail.com>
2024-03-15 14:47:26 +0530

    added about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div className="max-w-6xl mx-auto p-3 flex flex-col space-y-4">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+      <p>
+        Welcome to our movie database website! We are dedicated to providing you
+        with a comprehensive collection of movies from around the world. Whether
+        you're a cinephile or just looking for something new to watch, our
+        database has something for everyone.
+      </p>
+
+      <p>
+        Our website features a vast array of movies, ranging from classic films
+        to the latest releases. You can explore different genres, discover
+        hidden gems, and read reviews to help you decide what to watch next. Our
+        goal is to make it easy for you to find and enjoy great movies, no
+        matter what your tastes may be.
+      </p>
+
+      <p>
+        In addition to movie listings, we also provide information about actors,
+        directors, and other film industry professionals. You can learn more
+        about your favorite stars, their filmography, and even discover new
+        talent to watch out for.
+      </p>
+
+      <p>
+        We are constantly updating our database to ensure that you have access
+        to the latest information. Whether you're looking for a specific movie
+        or just browsing for something new, our movie database is your ultimate
+        source for all things film-related.
+      </p>
+
+      <p>
+        Thank you for visiting our website, and we hope you enjoy exploring our
+        collection of movies!
+      </p>
+    </div>
+  );
+}
